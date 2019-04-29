75191ac3be9410ab7d1776fbb4891e7966aec2b4
Jim <jimtrinidad002@gmail.com>
2019-04-30 00:17:32 +0800

    menu

--- application/main/views/templates/menu.php
+++ application/main/views/templates/menu.php
@@ -9,19 +9,20 @@
 	<?php echo ($pageSubTitle ?? 'Ambilis Mobile Platform'); ?>
 	
 	<div class="dropmenu">
-		<ul>
-			<li><a href="<?php echo site_url('account')?>">My Account</a></li>
-			<li><a href="<?php echo site_url('ewallet')?>">eWallet</a></li>
-			<li><a href="<?php echo site_url('rewards')?>">Rewards</a></li>
-			<li><a href="<?php echo site_url('connections')?>">Connections</a></li>
-			<li><a href="<?php echo site_url('marketplace')?>">Marketplace</a></li>
-			<li><a href="<?php echo site_url('bills')?>">Bills Payment</a></li>
-			<li><a href="javascript:;" onclick="Wallet.moneyPadalaRequest()">Money Padala</a></li>
-			<li><a href="javascript:;" onclick="Wallet.eloadRequest()">eLoad</a></li>
-			<li><a href="<?php echo site_url('store')?>">My Business</a></li>
-			<li><a href="<?php echo site_url('support')?>">Support</a></li>
-			<li><a href="<?php echo site_url('howitworks')?>">How it Works</a></li>
-			<li><a href="<?php echo site_url('terms')?>">Terms & Conditions</a></li>
+		<ul class="m-0 p-0">
+			<li><a href="<?php echo site_url()?>"><img src="<?php echo public_url(); ?>resources/images/menu/cat.png"/>Categories</a></li>
+			<li><a href="<?php echo site_url('account')?>"><img src="<?php echo public_url(); ?>resources/images/menu/my-account.png"/>My Account</a></li>
+			<li><a href="<?php echo site_url('ewallet')?>"><img src="<?php echo public_url(); ?>resources/images/menu/ewallet.png"/>eWallet</a></li>
+			<li><a href="<?php echo site_url('rewards')?>"><img src="<?php echo public_url(); ?>resources/images/menu/rewards.png"/>Rewards</a></li>
+			<li><a href="<?php echo site_url('connections')?>"><img src="<?php echo public_url(); ?>resources/images/menu/conn.png"/>Connections</a></li>
+			<li><a href="<?php echo site_url('marketplace')?>"><img src="<?php echo public_url(); ?>resources/images/menu/marketplace.png"/>Marketplace</a></li>
+			<li><a href="<?php echo site_url('bills')?>"><img src="<?php echo public_url(); ?>resources/images/menu/bills.png"/>Bills Payment</a></li>
+			<li><a href="javascript:;" onclick="Wallet.moneyPadalaRequest()"><img src="<?php echo public_url(); ?>resources/images/menu/money-transfer.png"/>Money Padala</a></li>
+			<li><a href="javascript:;" onclick="Wallet.eloadRequest()"><img src="<?php echo public_url(); ?>resources/images/menu/eload.png"/>eLoad</a></li>
+			<li><a href="<?php echo site_url('store')?>"><img src="<?php echo public_url(); ?>resources/images/menu/mybusiness.png"/>My Business</a></li>
+			<li><a href="<?php echo site_url('support')?>"><img src="<?php echo public_url(); ?>resources/images/menu/support.png"/>Support</a></li>
+			<li><a href="<?php echo site_url('howitworks')?>"><img src="<?php echo public_url(); ?>resources/images/menu/how.png"/>How it Works</a></li>
+			<li><a href="<?php echo site_url('terms')?>"><img src="<?php echo public_url(); ?>resources/images/menu/terms.png"/>Terms & Conditions</a></li>
 		</ul>
 	</div>
 
