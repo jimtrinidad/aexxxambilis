93a09b7a7976132d1c24886dae656e7d9c583e97
Jim <jimtrinidad002@gmail.com>
2019-06-23 18:19:05 +0800

    favicon

--- application/main/views/account/registration.php
+++ application/main/views/account/registration.php
@@ -5,6 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="<?php echo public_url(); ?>resources/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo public_url(); ?>resources/images/favicon.ico" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap-reboot.min.css" />
